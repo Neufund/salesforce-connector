b3a0f376a4e91d441fcdcbff0015823943af4171
Leonid Logvinov <logvinov.leon@gmail.com>
2017-03-29 11:38:20 +0200

    Return KYC data

--- config.py
+++ config.py
@@ -1,8 +1,20 @@
 SF_INSTANCE = "eu11.salesforce.com"
 SF_USERNAME = "remco+salesforce-api@neufund.org"
 
-PUBLIC_FIELDS = ["LastName", "FirstName", "Name"]
-PRIVATE_FIELDS = ["Phone", "Fax", "MobilePhone", "Email", "Ethereum_Address__c"]
+PUBLIC_FIELDS = ["LastName",
+                 "FirstName",
+                 "Name"]
+PRIVATE_FIELDS = ["Phone",
+                  "Fax",
+                  "MobilePhone",
+                  "Email",
+                  "Ethereum_Address__c",
+                  "KYC_Identification__c",
+                  "KYC_Address__c",
+                  "KYC_Submitted__c",
+                  "KYC_Accepted__c",
+                  "KYC_Rejected__c",
+                  "KYC_Response__c"]
 REQUIRED_ENV_CONFIG_FIELDS = ["SF_PASSWORD", "SF_TOKEN"]
 
 MS2_AUDIENCE = "MS2"
